f1770a6066c41eb6f384703afac4401bc69ebbbc
ArchLeaders <archparliament@gmail.com>
2023-04-01 14:59:37 -0700

    Remove unused source generator

--- autogen/Models/AliasesModel.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Text.Json.Serialization;
-
-namespace FontAwesome.Generate.Models;
-
-public class AliasesModel
-{
-    [JsonPropertyName("names")]
-    public List<string> Names { get; set; } = new();
-}

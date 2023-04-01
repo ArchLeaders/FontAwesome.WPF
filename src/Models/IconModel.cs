5b58cbb9d77b124fc1a6f3ac64acba3fa8712284
ArchLeaders <archparliament@gmail.com>
2023-04-01 15:04:53 -0700

    Add IconModel

---
+++ src/Models/IconModel.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.Json.Serialization;
+
+namespace FontAwesome.WPF.Models;
+
+public class IconModel
+{
+    public string this[string brand] {
+		get => Svg[brand].Path;
+	}
+
+	[JsonPropertyName("unicode")]
+    public required string Unicode { get; set; }
+
+	[JsonPropertyName("svg")]
+    public required Dictionary<string, SvgModel> Svg { get; set; }
+
+	[JsonPropertyName("styles")]
+    public required List<string> Styles { get; set; }
+
+    public class SvgModel
+    {
+        [JsonPropertyName("path")]
+        public required string Path { get; set; }
+    }
+}

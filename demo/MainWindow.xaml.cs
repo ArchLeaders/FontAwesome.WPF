0cc3c62493bd5a8e0a96b4754b5c0bab76f5c786
ArchLeaders <archleadership28@gmail.com>
2023-12-30 14:49:12 -0800

    Update demo

--- demo/MainWindow.xaml.cs
+++ demo/MainWindow.xaml.cs
@@ -9,14 +9,5 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
-        DataContext = new MainWindowViewModel();
-    }
-
-    private void Button_Click(object sender, RoutedEventArgs e)
-    {
-        (DataContext as MainWindowViewModel)!.Icon = "circle-stop:regular";
-        // icon0.IconName = "cloud-sun-rain";
-        // icon0.IconType = "solid";
-        // icon0.Rotation = 90;
     }
 }
--- demo/MainWindowViewModel.cs
+++ demo/MainWindowViewModel.cs
@@ -5,5 +5,5 @@ namespace FontAwesome.WPF.Demo;
 public partial class MainWindowViewModel : ObservableObject
 {
     [ObservableProperty]
-    private string icon = "github";
+    private string _icon = "github";
 }
